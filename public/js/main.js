96d3a78c48bd5c321feeff76cb609efebec5e55a
KAHMOOSHA <brhmhelou@gmail.com>
2022-02-23 14:44:45 +0200

    refactor the code

--- public/js/main.js
+++ public/js/main.js
@@ -25,22 +25,4 @@ const createImage = (tag, nameClass = '', alt = 'image', src = '#') => {
   element.src = src;
   return element;
 };
-// render data for film
-// eslint-disable-next-line no-unused-vars
-const renderData = (data) => {
-  const wholeContainer = getElement('.film-property');
-  deleteChild(wholeContainer);
-  data.forEach((film) => {
-    const boxCont = createElement('div', 'box-film');
-    const figure = createElement('figure');
-    const image = createImage('img', 'image-film', 'cover', film.large_cover_image);
-    figure.appendChild(image);
-    const infoCont = createElement('div', 'info');
-    const title = createElement('h2', 'film-name', film.title);
-    const rating = createElement('p', 'desc-film', `${film.rating}/10`);
-    const categorty = createElement('p', 'desc-film', film.genres);
-    infoCont.append(title, rating, categorty);
-    boxCont.append(figure, infoCont);
-    wholeContainer.appendChild(boxCont);
-  });
-};
+
